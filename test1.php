c1d4f4a828cfd0e252152069100fe709694de4b7
Kukkai15 <pasika.yap@gmail.com>
2023-06-06 11:53:52 +0700

    ข้อที่1

---
+++ test1.php
@@ -0,0 +1,10 @@
+<?php
+### 1.สร้าง array ที่มีชื่อเมือง 5 เมือง และแสดงผลทั้งหมด
+
+$array1 = array('Tokyo' ,'Bangkok', 'Paris', 'London', 'New York');
+for ($i=0; $i<count($array1); $i++) {
+    echo $array1[$i],'&nbsp;';
+}
+
+
+?>
